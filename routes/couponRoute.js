354b0c145fc387ba5e67e7c35dd91ba609ab2b7c
iamindranil <indranilchakraborty692@gmail.com>
2023-10-14 22:53:10 +0530

    final commited

--- routes/couponRoute.js
+++ routes/couponRoute.js
@@ -6,7 +6,7 @@ const router=express.Router();
 router.post('/',authMiddleware,isAdmin,createCoupon);
 router.post('/',authMiddleware,isAdmin,getAllCoupon);
 router.put('/:id',authMiddleware,isAdmin,updateCoupon);
-// router.delete('/',authMiddleware,isAdmin,getAllCoupon);
+router.delete('/',authMiddleware,isAdmin,getAllCoupon); 
 
 
 module.exports=router;
